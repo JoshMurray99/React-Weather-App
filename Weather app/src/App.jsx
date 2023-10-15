d4fcc34d95907833831b384b187bd2f41d15ecb1
Joshua Murray <115622052+JoshMurray99@users.noreply.github.com>
2023-10-15 22:15:10 +0100

    added forecast and search functionality

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -1,45 +1,100 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { fetchWeatherData } from '../Hooks/FetchWeatherData';
+
+import CurrentWeather from './components/CurrentWeather';
+import HourlyForecast from './components/HourlyForecast';
+import DailyForecast from './components/DailyForecast';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const API_KEY = import.meta.env.API_KEY
-  const cityName = 'London'
-  console.log(API_KEY)
+  const [citySearch, setCitySearch] = useState('');
+  const [cityOptions, setCityOptions] = useState([]);
+  const [showList, setShowList] = useState(true);
+  const [weatherData, setWeatherData] = useState(null)
 
-  async function getWeather() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const handleSelection = (city, state, lon, lat) => {
+    setCitySearch(`${city}, ${state}`); 
+    setShowList(false);
+    getWeatherData(lat, lon)
+  }
+
+
+  async function getCities() {
+    setShowList(true);
     const options = {
       method: 'GET',
       url: 'http://api.openweathermap.org/geo/1.0/direct',
       params: {
-      q: `${cityName}`,
-      limit: 1,
-      appid: API_KEY,
-    },
+        q: `${citySearch}`,
+        limit: 5,
+        appid: API_KEY,
+      },
+    };
+    try {
+      const response = await axios.request(options);
+      const cityNames = response.data.map((city) => ({
+        name: city.name,
+        state: city.state,
+        lat: city.lat,
+        lon: city.lon
+      }));
+      setCityOptions(cityNames);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  try {
-    const response = await axios.request(options);
-    console.log(response.data)
+  async function getWeatherData(lat, lon) {
+    const data = await fetchWeatherData(lat, lon)
+    setWeatherData(data)
+    
   }
-  catch(error){
-    console.log(error);
-  }
-}
-
-// useEffect(() => {
-//   getWeather()
-// }, [])
 
   return (
     <>
-      <div>
-     Hi
-      </div>
-      
+        <div className='search'>
+          <TextField
+            id="outlined-basic"
+            label="City"
+            variant="outlined"
+            onChange={(e) => setCitySearch(e.target.value)}
+            value={citySearch}
+          />
+          <Button variant="contained" onClick={getCities}>
+            Search
+          </Button>
+        </div>
+        {cityOptions.length > 0 && showList && (
+          <List>
+            {cityOptions.map((city, index) => (
+              <ListItem key={index} disablePadding>
+                <ListItemButton onClick={() => handleSelection(city.name, city.state, city.lon, city.lat)}>
+                  <ListItemText primary={city.name} secondary={city.state} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        )}
+
+        
+        {weatherData && (
+          <>
+          <CurrentWeather data={weatherData} city={citySearch}/>
+          <HourlyForecast data={weatherData}  />
+          <DailyForecast data={weatherData} />
+          </>
+        )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Weather app/src/components/HourlyForecast/index.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import { Grid, Box } from '@mui/material';
+
+export default function HourlyForecast({ data }) {
+    const next5Hours = data.hourly.slice(0, 5).map((hourlyItem) => ({
+        time: new Date((hourlyItem.dt + data.timezone_offset - 3600) * 1000).toLocaleTimeString(),
+        temp: hourlyItem.temp,
+        icon: hourlyItem.weather[0].icon,
+    }));
+
+    return (
+        <div>
+
+            <Box sx={{ display: 'flex', mt: 5 }}>
+
+                {next5Hours.map((hour, index) => (
+                    <Grid key={index} >
+                        <Card variant="outlined">
+                            <CardContent>
+                                <Typography variant="h6" component="div">
+                                    {hour.time.slice(0, 5)}
+                                </Typography>
+
+                                <CardMedia
+                                    component="img"
+                                    height="100"
+                                    image={`https://openweathermap.org/img/wn/${hour.icon}@2x.png`}
+                                    alt={`Weather icon for the hour`}
+                                />
+                                <Typography variant="h6">
+                                    {Math.round(hour.temp)} °C
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
+
+            </Box>
+        </div>
+
+    )
+}
